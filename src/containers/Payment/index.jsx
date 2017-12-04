28d4de11d1533d42a38a9b39a3eeceef7f393f97
Rodrigo Spirolazzi <rspirolazzi@gmail.com>
2017-12-03 22:09:19 -0300

    Payment

--- src/containers/Payment/index.jsx
+++ src/containers/Payment/index.jsx
@@ -1,41 +1,23 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-import {Input, Row, Col, CardPanel, Button} from 'react-materialize'
-const Payment = ({match})=> {
-    const table = [
-        {plan:1, inc:1.00},
-        {plan:3, inc:1.10},
-        {plan:6, inc:1.15},
-        {plan:9, inc:1.20},
-        {plan:12, inc:1.25},
-    ]
-    const {price} = match.params
-    const renderOptions=(plan)=>{
-        let label = plan === 1 ?'cuota':'cuotas'
-        let result = table.filter(i=>i.plan === plan).shift()
-        let {inc} = result
-        let total = parseInt(price*inc, 0)
-
-        return <option key={plan} value={plan}>{plan} {label} de ${total}</option>
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import PaymentItem from '../../components/Payment/'
+import {getProduct} from '../../actions/products'
+class Payment extends Component {
+    componentWillMount(){
+        this.props.dispatch(getProduct(this.props.match.params.id))
     }
-    return <Row>
-        <Col s={12} m={12}>
-            <h4>Pagar con tarjeta de credito ${price}</h4>
-            <CardPanel className="lighten-4">
-                <Row>
-                    <Input s={12} label="Card Number"/>
-                    <Input s={6} label="Expiration Date"/>
-                    <Input s={6} label="CVC"/>
-                    <Input s={12} type='select' label="Cuotas" defaultValue='1'>
-                        {[1,3,6,9,12].map(renderOptions)}
-                    </Input>
-                    <Button className="col s12 blue">Confirmar</Button>
-                    <Col s={6} m={6} l={6}>
-                        <Link to={'/'}>Volver</Link>
-                    </Col>
-                </Row>
-            </CardPanel>
-        </Col>
-    </Row>
+    render() {
+        const {item} = this.props
+        if(item._id==null){
+            return null
+        }
+        return <PaymentItem item={item}/>
+    }
 }
-export default Payment
+const mapStateToProps = (state, ownProps)=> {
+    const {item} = state.products || {item: {}}
+    return {
+        item
+    }
+}
+export default connect(mapStateToProps)(Payment)
